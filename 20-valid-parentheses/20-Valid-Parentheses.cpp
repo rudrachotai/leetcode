5320ed87050587527cb9e4ee178b05842b7d12bf
rudrachotai <rudrachotai234@gmail.com>
2026-09-02 22:19:36 +0530

    [20] [Time Beats: 100.00%] [Memory Beats: 37.42%] - LeetPush

---
+++ 20-valid-parentheses/20-Valid-Parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack <char> st;
+       for(int i=0;i<s.size();i++){
+        if(s[i]=='('||s[i]=='['||s[i]=='{') st.push(s[i]);
+        else{
+            if(st.size()==0) return false;
+            else{
+                if(s[i]==')'&&st.top()=='('){
+                    st.pop();
+                    continue;
+                }
+                if(s[i]==']'&&st.top()=='[') {
+                    st.pop();
+                    continue;
+                    }
+                if(s[i]=='}'&&st.top()=='{') {
+                    st.pop();
+                     continue;
+                }
+                return false;
+            }
+        }
+       }
+       if(st.size()!=0) return false;
+       return true; 
+    }
+};
