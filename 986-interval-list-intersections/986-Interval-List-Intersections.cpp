c0205cbac27892913e258dc750c69adcba92ea42
rudrachotai <rudrachotai234@gmail.com>
2026-09-16 17:21:26 +0530

    [986] [Time Beats: 56.00%] [Memory Beats: 58.32%] - LeetPush

--- 986-interval-list-intersections/986-Interval-List-Intersections.cpp
+++ 986-interval-list-intersections/986-Interval-List-Intersections.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-     vector<vector<int>> ans;
-     int i=0;
-     int j=0;
-     while(i<firstList.size()&&j<secondList.size()){
-    int start1 = firstList[i][0];
-    int end1 = firstList[i][1];
-    int start2 = secondList[j][0];
-    int end2 = secondList[j][1];
-    if(start1<=start2){
-   if(end1>=start2){int s = max(start1,start2);
-    int e = min(end1,end2);
-    ans.push_back({s,e});}
-    }
-    else{
-        if(end2>=start1){
-            int s = max(start1,start2);
-            int e = min(end1,end2);
+       vector<vector<int>> ans;
+       int i=0;
+       int j=0;
+       while(i<firstList.size() && j<secondList.size()){
+        int s1 = firstList[i][0];
+        int e1 = firstList[i][1];
+        int s2 = secondList[j][0];
+        int e2 = secondList[j][1];
+       if(e1>=s2 && s1<=s2){
+            int s = max(s1,s2);
+            int e = min(e1,e2);
             ans.push_back({s,e});
         }
-    }
-    if(end1<=end2) i++;
-    if(end2<end1) j++;
-
-     }
-     return ans;
+        else if(e2>=s1 && s1>=s2 ){
+            int s = max(s1,s2);
+            int e = min(e1,e2);
+            ans.push_back({s,e});
+        }
+         if(e1<=e2) i++;
+         if(e2<e1) j++;
+       }
+       return ans; 
     }
 };
